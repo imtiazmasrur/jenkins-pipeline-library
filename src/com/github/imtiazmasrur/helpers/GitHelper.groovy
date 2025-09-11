dbbe7e766a931d87305a145983fe01a422477a7d
Imtiaz Masrur <imtiazmasrur@Imtiazs-MacBook-Air.local>
2025-09-12 01:00:40 +0600

    test: current git tag

--- src/com/github/imtiazmasrur/helpers/GitHelper.groovy
+++ src/com/github/imtiazmasrur/helpers/GitHelper.groovy
@@ -4,26 +4,25 @@ package com.github.imtiazmasrur.helpers
  * Utility class for Git related operations such as fetching tags.
  */
 class GitHelper implements Serializable {
+    // def script
 
-    def script
-
-    GitHelper(script) {
-        this.script = script
-    }
+    // GitHelper(script) {
+    //     this.script = script
+    // }
 
     // Function to get the current Git tag
     String getCurrentTag() {
-        return script.sh(script: 'git describe --tags --abbrev=0', returnStdout: true).trim()
+        return sh(script: 'git describe --tags --abbrev=0', returnStdout: true).trim()
     }
 
     // Function to get the latest Git tag
     String getLatestTag() {
-        return script.sh(script: 'git tag -l --sort=-creatordate | head -n 1', returnStdout: true).trim()
+        return sh(script: 'git tag -l --sort=-creatordate | head -n 1', returnStdout: true).trim()
     }
 
     // Function to get before last one tag
     String getBeforeLastTag() {
-        return script.sh(script: 'git tag -l --sort=-creatordate | sed -n "2p"', returnStdout: true).trim()
+        return sh(script: 'git tag -l --sort=-creatordate | sed -n "2p"', returnStdout: true).trim()
     }
 
 }
--- vars/pm2Deploy.groovy
+++ vars/pm2Deploy.groovy
@@ -2,7 +2,8 @@ import com.github.imtiazmasrur.helpers.GitHelper
 import com.github.imtiazmasrur.helpers.NodeJSHelper
 
 def call(Map config) {
-    def gitHelper = new GitHelper(this)
+    // def gitHelper = new GitHelper(this)
+    def gitHelper = new GitHelper()
     // def nodeJSHelper = new NodeJSHelper(config.nodeJSVersion, config.nodeJSPath)
     
     // Get the current Git tag
