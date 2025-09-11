6a990cad0e892f453e3dffb51628b16948a94fb8
Imtiaz Masrur <imtiazmasrur@Imtiazs-MacBook-Air.local>
2025-09-12 02:30:12 +0600

    test: pm2 deployments

--- vars/pm2Deploy.groovy
+++ vars/pm2Deploy.groovy
@@ -3,7 +3,8 @@ import com.github.imtiazmasrur.deployments.pm2Deployment
 def call(Map config) {
     def pm2 = new pm2Deployment(this, config.nodeJSVersion, config.nodeJSPath, config.projectName, config.projectDirectory)
 
-    echo pm2.getStatus()
+    def status = pm2.getStatus()
+    echo "Deployment Status: ${status}"
 
     // Get the current Git tag
     // def currentTag = pm2.gitHelper.getCurrentTag()
