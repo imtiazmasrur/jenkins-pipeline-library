eb334066d4711d6cc5cee58ac7c8a2df2d45ae77
Imtiaz Masrur <im_oli106@yahoo.com>
2025-09-12 11:10:26 +0600

    test: nodejs deployment with stage

--- vars/nodeJSDeploy.groovy
+++ vars/nodeJSDeploy.groovy
@@ -3,40 +3,42 @@ import com.github.imtiazmasrur.deployments.NodeJSDeployment
 def call(Map config) {
     def nodeJS = new NodeJSDeployment(this, config)
 
-    stage('Check Git Status, CheckOut Latest Tag...') {
-        steps {
-            script {
-                nodeJS.checkoutCode()
+    node {
+        stage('Check Git Status, CheckOut Latest Tag...') {
+            steps {
+                script {
+                    nodeJS.checkoutCode()
+                }
             }
         }
-    }
-    stage('Deployment Started...') {
-        when {
-            expression { return nodeJS.DEPLOYMENT_STATUS }
-        }
-        steps {
-            script {
-                nodeJS.deploy()
+        stage('Deployment Started...') {
+            when {
+                expression { return nodeJS.DEPLOYMENT_STATUS }
+            }
+            steps {
+                script {
+                    nodeJS.deploy()
+                }
             }
         }
-    }
-    stage('Health Check...') {
-        when {
-            expression { return nodeJS.DEPLOYMENT_STATUS && !nodeJS.ROLLBACK_STATUS }
-        }
-        steps {
-            script {
-                nodeJS.healthCheck()
+        stage('Health Check...') {
+            when {
+                expression { return nodeJS.DEPLOYMENT_STATUS && !nodeJS.ROLLBACK_STATUS }
+            }
+            steps {
+                script {
+                    nodeJS.healthCheck()
+                }
             }
         }
-    }
-    stage('Rollback Started...') {
-        when {
-            expression { return nodeJS.ROLLBACK_STATUS }
-        }
-        steps {
-            script {
-                nodeJS.rollback()
+        stage('Rollback Started...') {
+            when {
+                expression { return nodeJS.ROLLBACK_STATUS }
+            }
+            steps {
+                script {
+                    nodeJS.rollback()
+                }
             }
         }
     }
